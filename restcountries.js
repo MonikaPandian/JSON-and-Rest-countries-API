085031446a603c2043c94a63844c08008555a522
MonikaPandian <87970544+MonikaPandian@users.noreply.github.com>
2022-06-18 18:46:58 +0530

    Rest-countries flags in console

---
+++ restcountries.js
@@ -0,0 +1,24 @@
+//Step 1:creating an XHR(XML-HTTP Request) object
+//XML-HTTP Request are a way to interact with the servers via API
+//API is restcountries
+var request=new XMLHttpRequest();
+//Step 2:create a request/open a request
+//open is an inbuilt method.open method consists of two parameters:HTTP method,API-URL
+request.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
+//Step 3:creating a bridge
+request.send();
+//Step 4:Status code for success:200
+//once the data successfully received from the server
+//the data travelling from the server
+request.onload=function(){
+    var result=JSON.parse(request.response);
+ 
+  
+   //Iterating through for loop
+for(var i=0;i<result.length;i++){
+  console.log(result[i].name+":"+result[i].flag);
+ 
+}
+
+
+}
